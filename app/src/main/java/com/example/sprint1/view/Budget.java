dd9005c9b322c3db4fd03cff172dede613d9c513
Showmick Das <showmickdas75@gmail.com>
2025-10-03 22:39:34 -0400

    Add 4 new Pages

--- app/src/main/java/com/example/sprint1/view/Budget.java
+++ app/src/main/java/com/example/sprint1/view/Budget.java
@@ -1,4 +1,21 @@
 package com.example.sprint1.view;
 
-public class Budget {
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.sprint1.R;
+
+public class Budget extends Fragment {
+
+    public Budget() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_budgets, container, false);
+    }
 }
