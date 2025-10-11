82b7b9d6aff23c2db07299e9c2ac519f0d57b438
Aazam Alam <aazam.alam.aa@gmail.com>
2025-10-11 11:08:29 -0400

    ViewModel and models created for Expense

--- app/src/main/java/com/example/spendwise/viewModel/ExpenseViewModel.java
+++ app/src/main/java/com/example/spendwise/viewModel/ExpenseViewModel.java
@@ -1,4 +1,24 @@
 package com.example.spendwise.viewModel;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ExpenseViewModel {
+    private final MutableLiveData<String> logResult = new MutableLiveData<>();
+
+    public ExpenseViewModel() {}
+
+    public LiveData<String> getLoginResult() {
+        return logResult;
+    }
+
+    public void addExpense(String name, String amount, String category, String date) {
+        if (name.isEmpty() || amount == null ||date.isEmpty()) {
+            logResult.setValue("Please enter valid data in the input fields");
+            return;
+        }
+
+    }
 }
