d956b9577c54b7ec0624be9993affa4a4b9b98b1
Aazam Alam <aazam.alam.aa@gmail.com>
2025-11-05 23:46:49 -0500

    flakdf

--- Code_Smells/EmailSender.java
+++ Code_Smells/EmailSender.java
@@ -4,8 +4,5 @@ public class EmailSender {
    }
    
     public static void sendEmail(String customerEmail, String subject, String message){
-        System.out.println("Email to: " + customerEmail);
-        System.out.println("Subject: " + subject);
-        System.out.println("Body: " + message);
     }
 }
