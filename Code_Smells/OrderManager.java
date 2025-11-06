be001cb81233d02d2655b489848312b6e2169c19
Aazam Alam <aazam.alam.aa@gmail.com>
2025-11-06 00:34:22 -0500

    Refactor logging and email formatting in OrderManager

--- Code_Smells/OrderManager.java
+++ Code_Smells/OrderManager.java
@@ -1,19 +1,22 @@
 import java.util.logging.Logger;
+import java.text.MessageFormat;
 
 public class OrderManager {
     private static final Logger logger = Logger.getLogger(OrderManager.class.getName());
     
     // FIX 6: Extracted printing responsibility
     public void printOrder(Order order) {
-        logger.info(orderToString(order));
+        logger.info("{0}", orderToString(order));
     }
-
+    
     // FIX 6: Extracted email responsibility
     public void sendConfirmationEmail(Order order) {
-        String message = "Thank you for your order, " + order.getCustomerName() + "!\n\n" + orderToString(order);
+        String message = MessageFormat.format("Thank you for your order, {0}!\n\n{1}", 
+                                              order.getCustomerName(), 
+                                              orderToString(order));
         EmailSender.sendEmail(order.getCustomerEmail(), "Order Confirmation", message);
     }
-
+    
     public String orderToString(Order order) {
         StringBuilder str = new StringBuilder("Order Details:\n");
         for (Item item : order.getItems()) {
