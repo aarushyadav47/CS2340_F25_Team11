896ac0d751551dd47e7181512ceb6cc74b58fbf0
Aazam Alam <aazam.alam.aa@gmail.com>
2025-11-06 00:41:10 -0500

    Refactor logging to use MessageFormat for total price

--- Code_Smells/Main.java
+++ Code_Smells/Main.java
@@ -1,4 +1,5 @@
 import java.util.logging.Logger;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,17 +11,17 @@ public class Main {
         Item item1 = new Item("Book", 20, 1, new AmountDiscount(5));
         Item item2 = new TaxableItem("Laptop", 1000, 1, new PercentageDiscount(0.1));
         Item item3 = new GiftCardItem("Gift Card", 10, 1, new AmountDiscount(0));
-
+        
         List<Item> items = new ArrayList<>();
         items.add(item1);
         items.add(item2);
         items.add(item3);
-
+        
         Order order = new Order(items, "John Doe", "johndoe@example.com");
         OrderManager orderManager = new OrderManager();
-
+        
         logger.info("\n########## Calculating Total #############");
-        logger.info("Total Price: ${0}", order.calculateTotalPrice());
+        logger.info(MessageFormat.format("Total Price: ${0}", order.calculateTotalPrice()));
         
         logger.info("\n########## Sending Email #############");
         orderManager.sendConfirmationEmail(order);
